0efc46c5896eeaeb3ac5f13a2ca69392e9a01d93
Pavel <paha_gagarin@mail.ru>
2023-06-13 20:50:14 +0300

    подключение малтер для обработки картинок на сервере

--- routes/planes.js
+++ routes/planes.js
@@ -1,13 +1,24 @@
 const express = require('express');
 const router = express.Router();
 const path = require('path');
-const { getPlanes } = require('../controllers/planes');
+const { getPlanes, createPlane } = require('../controllers/planes');
+const multer = require('multer');
+
+//показываем, где хранить загруженные файлы
+const storage = multer.diskStorage({
+  destination: './assets/',
+  filename: (req, file, cb) => {
+    cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname))
+  }
+});
+
+const upload = multer({ storage });
 
 // @route get /api/planes - все самолеты
 router.get('/', getPlanes);
 // @route get /api/planes/:id - получить самолет по id
 router.get('/:id', (req, res) => res.send('get single planes'));
 // @route post /api/planes - создать самолет
-router.post('/', (req, res) => res.send('create plane'));
+router.post('/', upload.single('planeImage'), createPlane);
 
 module.exports = router;
